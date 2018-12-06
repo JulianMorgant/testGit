f5064b448940574bf12039ab6fcc0a696c9388e3
Julian <julian.morgant@yahoo.fr>
2018-12-06 13:07:34 +0100

    commit 2

---
+++ exo1.js
@@ -0,0 +1,10 @@
+function entierAleatoire(min, max)
+{
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//Utilisation
+//La variable contient un nombre aléatoire compris entre 1 et 10
+var entier = entierAleatoire(0, 1000);
+var index = 0;
+while (index != entierAleatoire*(0,1000)) {index ++};
+console.log (index); 
